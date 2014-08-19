d1a94c23654a6d9377eefc3a1700965e4c15220e
Brian <brian.lamarche@gmail.com>
2014-08-19 11:25:17 -0700

    changed colors

--- LedStrip/ledstripSimple.py
+++ LedStrip/ledstripSimple.py
@@ -12,23 +12,27 @@ leds.allOff()
 
 # then simply color things
 # colors range from 0 to 255 (the size of a byte)
-leds.colorLed(0, 255, 	0,   0)		# first 	is red
-leds.colorLed(1, 0,      255,   0)		# second 	is green
-leds.colorLed(2, 0, 	 	0,   255)		# third 	is blue
+leds.colorLed(0, 255, 	127,   0)		# first 	is red
+leds.colorLed(1, 255, 	127,   0)		# first 	is red
+leds.colorLed(2, 255, 	127,   0)		# first 	is red
+leds.colorLed(3, 55, 	127,   0)		# first 	is red
+leds.colorLed(4, 25, 	127,   0)		# first 	is red
+leds.colorLed(5, 25, 	127,   0)		# first 	is red
+
 leds.writeAll()
 time.sleep(1)
 
 # colors range from 0 to 255 (the size of a byte)
 leds.colorLed(0, 0, 	 	0,   255)		# first 	is blue
-leds.colorLed(1, 255, 	0,   0)		# second 	is red
-leds.colorLed(2, 0,      255,   0)		# third 	is green
+leds.colorLed(1, 0, 	 	0,   255)		# first 	is blue
+leds.colorLed(1, 0, 	 	0,   255)		# first 	is blue
 leds.writeAll()
 time.sleep(1)
 
 # colors range from 0 to 255 (the size of a byte)
 leds.colorLed(0, 0,      255,   0)		# first 	is green
-leds.colorLed(1, 0, 	 	0,   255)		# second 	is blue
-leds.colorLed(2, 255, 	0,   0)		# thid 	is red
+leds.colorLed(1, 0,      255,   0)		# first 	is green
+leds.colorLed(1, 0,      255,   0)		# first 	is green
 leds.writeAll()
 time.sleep(1)
 
